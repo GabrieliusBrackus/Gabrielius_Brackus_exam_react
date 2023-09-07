4d2877c2e7f5c7368748a48651d4c42145855210
Gabrielius Bračkus <brackus.gabrielius@gmail.com>
2023-09-07 12:41:17 +0300

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
         <Route path="/" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,15 @@
+export default function Footer() {
+  return (
+    <footer className="bg-neutral-100 text-center text-neutral-600 dark:bg-neutral-600 dark:text-neutral-200 lg:text-left fixed bottom-0 w-full">
+      <div className="bg-neutral-200 p-6 text-center dark:bg-neutral-700">
+        <span>© 2023 Copyright:</span>
+        <a
+          className="font-semibold text-neutral-600 dark:text-neutral-400"
+          href="/"
+        >
+          MY SHOP
+        </a>
+      </div>
+    </footer>
+  );
+}
