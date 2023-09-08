9710fed3c379ccc74c835905c279b6576f77d207
Gabrielius Bračkus <brackus.gabrielius@gmail.com>
2023-09-08 08:27:23 +0300

    Registration created

---
+++ src/components/auth/RegistrationForm.jsx
@@ -0,0 +1,138 @@
+import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import toast, { Toaster } from "react-hot-toast";
+
+export default function RegistrationForm() {
+  const navigate = useNavigate();
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+    validationSchema: Yup.object({
+      email: Yup.string()
+        .email("Invalid email format")
+        .required("Email is required"),
+      password: Yup.string()
+        .min(6, "Password must be at least 6 characters long")
+        .required("Password is required"),
+      confirmPassword: Yup.string()
+        .oneOf([Yup.ref("password"), null], "Passwords must match")
+        .required("Confirm password is required"),
+    }),
+    onSubmit: async (values) => {
+      try {
+        const { email, password } = values;
+        const auth = getAuth();
+        const userCredential = await createUserWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+
+        toast.success("Registration successful, welcome:" + email);
+
+        navigate("/shops", { replace: true });
+      } catch (error) {
+        toast.error("Registration failed, please try again");
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.warn({ errorCode, errorMessage });
+      }
+    },
+  });
+
+  return (
+    <div className="bg-gray-700 shadow-md rounded px-8 pt-6 pb-8 mb-4 text-white">
+      <div>
+        <Toaster />
+      </div>
+      <h1 className="text-2xl mb-4">Registration</h1>
+      <form onSubmit={formik.handleSubmit} className="mb-4">
+        <div className="mb-4">
+          <label
+            htmlFor="email"
+            className="block text-white-700 text-sm font-bold mb-2"
+          >
+            Email:
+          </label>
+          <input
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.email}
+            className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="text"
+            id="email"
+            name="email"
+            placeholder="Email"
+          />
+          {formik.errors.email && formik.touched.email && (
+            <p className="text-red-500 text-xs italic">{formik.errors.email}</p>
+          )}
+        </div>
+
+        <div className="mb-4">
+          <label
+            htmlFor="password"
+            className="block text-white-700 text-sm font-bold mb-2"
+          >
+            Password:
+          </label>
+          <input
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.password}
+            className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Password"
+          />
+          {formik.errors.password && formik.touched.password && (
+            <p className="text-red-500 text-xs italic">
+              {formik.errors.password}
+            </p>
+          )}
+        </div>
+
+        <div className="mb-4">
+          <label
+            htmlFor="confirmPassword"
+            className="block text-white-700 text-sm font-bold mb-2"
+          >
+            Confirm Password:
+          </label>
+          <input
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.confirmPassword}
+            className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="password"
+            id="confirmPassword"
+            name="confirmPassword"
+            placeholder="Confirm Password"
+          />
+          {formik.errors.confirmPassword && formik.touched.confirmPassword && (
+            <p className="text-red-500 text-xs italic">
+              {formik.errors.confirmPassword}
+            </p>
+          )}
+        </div>
+
+        <div className="flex items-center justify-between">
+          <button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          >
+            Register
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,8 +1,13 @@
+import React from "react";
+import RegistrationForm from "../components/auth/RegistrationForm";
 export default function RegisterPage() {
   return (
-    <div className="container">
-      <h2 className="text-3xl mb-8">Register Page</h2>
-      <p>Welcome to Register page</p>
+    <div className="flex flex-col justify-center items-center">
+      <div className="max-w-md w-full p-6 bg-gray-800 rounded-md shadow-md mt-5 text-white">
+        <h2 className="text-3xl mb-4 text-center">Register Page</h2>
+        <p className="text-center">To use our website, you must sign up.</p>
+        <RegistrationForm />
+      </div>
     </div>
   );
 }
