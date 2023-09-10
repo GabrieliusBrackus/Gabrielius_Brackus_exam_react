2b1c7b6441411d7e97969b2b3e1259db15d1a02e
Gabrielius Bračkus <brackus.gabrielius@gmail.com>
2023-09-10 13:13:28 +0300

    shop item delete added

--- src/components/shop/SingleItem.jsx
+++ src/components/shop/SingleItem.jsx
@@ -1,14 +1,35 @@
 import React from "react";
+import { deleteDoc, doc } from "firebase/firestore";
+import { db } from "../../firebase/firebase";
+import toast, { Toaster } from "react-hot-toast";
 
 export default function SingleItem(props) {
   const { item, currentUserUid } = props;
-
+  const isOnSale = item.saleOnSale;
+  const price = item.price;
+  const discountedPrice = isOnSale ? price * 0.5 : price;
+  const handleDelete = async () => {
+    try {
+      if (currentUserUid === item.userUid) {
+        const itemRef = doc(db, "shops", item.id);
+        await deleteDoc(itemRef);
+        toast.success("Shop item deleted successfully.");
+      } else {
+        toast.error("You do not have permission to delete this item.");
+      }
+    } catch (error) {
+      console.error("Error deleting item:", error);
+    }
+  };
   return (
     <li
       className={`border p-4 ${
-        item.userUid !== currentUserUid ? "bg-yellow-100" : ""
+        item.userUid === currentUserUid ? "bg-yellow-100" : ""
       }`}
     >
+      <div>
+        <Toaster />
+      </div>
       <div className="flex flex-col items-center">
         <img
           src={item.imageUrl}
@@ -17,6 +38,26 @@ export default function SingleItem(props) {
           style={{ maxHeight: "150px" }}
         />
         <p className="text-xl font-semibold mt-2">{item.shopName}</p>
+
+        <div className="flex justify-between w-full">
+          <div>
+            {isOnSale ? (
+              <div className="text-red-500">
+                <del>{price.toFixed(2)}€</del> ({discountedPrice.toFixed(2)}€)
+              </div>
+            ) : (
+              <div>{price.toFixed(2)}€</div>
+            )}
+          </div>
+          {currentUserUid === item.userUid && (
+            <button
+              className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded focus:outline-none focus:shadow-outline"
+              onClick={handleDelete}
+            >
+              Delete
+            </button>
+          )}
+        </div>
       </div>
     </li>
   );
